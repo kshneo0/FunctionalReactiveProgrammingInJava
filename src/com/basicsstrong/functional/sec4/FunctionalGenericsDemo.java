e4782ef4d0c30998b63c8947cd2f882eff64c5ad
kshneo0 <kshneo@gmail.com>
2022-03-27 21:45:00 +0900

    18. Generic Functional Interfaces

---
+++ src/com/basicsstrong/functional/sec4/FunctionalGenericsDemo.java
@@ -0,0 +1,15 @@
+package com.basicsstrong.functional.sec4;
+
+public class FunctionalGenericsDemo {
+
+	public static void main(String[] args) {
+
+		FunctionalGenerics<String, String> fun = s -> s.substring(1,5);
+		System.out.println(fun.execute("BasicsStrong"));
+		
+		FunctionalGenerics<String, Integer> fun1 = s -> s.length();
+		System.out.println(fun1.execute("BasicsStrong"));
+		
+	}
+	
+}
