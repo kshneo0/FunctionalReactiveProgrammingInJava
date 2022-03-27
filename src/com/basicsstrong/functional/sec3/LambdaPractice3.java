1e9b9a0bb442d691e497b3153ec2cc8e1a5f8e05
kshneo0 <kshneo@gmail.com>
2022-03-27 21:20:13 +0900

    14. Practice Lambda - 4

--- src/com/basicsstrong/functional/sec3/LambdaPractice3.java
+++ src/com/basicsstrong/functional/sec3/LambdaPractice3.java
@@ -4,10 +4,19 @@ public class LambdaPractice3 {
 
 	public static void main(String[] args) {
 		
-		LengthOfString len = str -> str.length();		
-		int length = len.length("BasicsStrong");
-		System.out.println(length);
+//		LengthOfString len = str -> str.length();
 		
+		LengthOfString len = str -> {
+			
+			int l = str.length();
+			
+			System.out.println("The length of the given String is "+ l);
+			
+			return l;
+			
+		};
+		int length = len.length("BasicsStrong");
+		System.out.println(length);		
 		
 	}
 }
