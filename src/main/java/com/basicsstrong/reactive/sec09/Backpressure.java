7c54cbf1aa095efbba3279e4edc15a58d5683bb6
kshneo0 <kshneo@gmail.com>
2022-04-01 13:44:23 +0900

    166. BackPressure With Flowable-Subscriber

--- src/main/java/com/basicsstrong/reactive/sec09/Backpressure.java
+++ src/main/java/com/basicsstrong/reactive/sec09/Backpressure.java
@@ -1,6 +1,5 @@
 package com.basicsstrong.reactive.sec09;
 
-import java.awt.desktop.ScreenSleepEvent;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.reactivestreams.Subscriber;
@@ -14,17 +13,56 @@ public class Backpressure {
 
 	public static void main(String[] args) {
 		
+		//Observable.range(1, 1000000)
 		Flowable.range(1, 1000000)
 		.map(e -> {
 			System.out.println("Produced item is : "+ e + " : "+ Thread.currentThread().getName());
 			return e;
 		})
 		.observeOn(Schedulers.io())
-		.subscribe( e -> {
-			sleep(100);
-			System.out.println("Consumed item is : "+ e +  " : "+ Thread.currentThread().getName());
-		} );
-		
+//		.subscribe( e -> {
+//			sleep(100);
+//			System.out.println("Consumed item is : "+ e +  " : "+ Thread.currentThread().getName());
+//		} );
+.subscribe( new Subscriber<Integer>() {
+			
+			Subscription s;
+			AtomicInteger count = new AtomicInteger(0);
+
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				System.out.println("Asking for 20 items");
+				s.request(20);
+				
+			}
+
+			@Override
+			public void onNext(Integer t) {
+				
+				
+				if(count.getAndIncrement() % 20 ==0) {
+					System.out.println("Asking for next 20 items ");
+					s.request(20);
+				}
+				
+				System.out.println("The subscriber consumed : "+ t);
+				sleep(100);
+				
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				
+			}
+
+			@Override
+			public void onComplete() {
+				System.out.println("Completed");
+				
+			}
+		});
 		sleep(100000000);
 
 	}
